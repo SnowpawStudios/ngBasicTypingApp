a02d6d88c6a6c71b1fd1666b9910319389b24257
SnowpawStudios <sayajjin@hotmail.com>
2022-10-12 06:38:23 +1000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {lorem } from 'faker';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'typing';
+  randomText = lorem.sentence();
+  enteredText = '';
+  
+
+  onInput(value: string) {   
+    this.enteredText = value;
+  }
+
+  compare(letter:string, enteredText:string){
+    if(!enteredText){
+      return 'pending';
+    }
+
+    return letter === enteredText ? 'correct' : 'incorrect';
+   
+  
+  }
+
+  
 }
